913781187d42b19a6378723b7e00bcc5990ce713
wilbrone <wilbroneokoth@gmail.com>
2020-03-02 12:42:29 +0300

    add component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { MainpageComponent } from './components/mainpage/mainpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {component: MainpageComponent, path: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

5d12174078117d4f6c81228f64dbe487b5c17ade
wilbrone <wilbroneokoth@gmail.com>
2020-03-18 11:37:40 +0300

    add section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +22,10 @@ import { MainpageComponent } from './components/mainpage/mainpage.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSliderModule,
+    MatProgressBarModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/mainpage/mainpage.component.ts
+++ src/app/components/mainpage/mainpage.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import {ThemePalette} from '@angular/material/core';
+// import {ProgressBarMode} from '@angular/material/progress-bar';
+
 @Component({
   selector: 'app-mainpage',
   templateUrl: './mainpage.component.html',
@@ -9,6 +12,19 @@ export class MainpageComponent implements OnInit {
 
   constructor() { }
 
+  // COME BACK AND RESTRUCTURE THIS SECTION 
+
+  color: ThemePalette = 'primary';
+  // mode: ProgressBarMode = 'determinate';
+  value = 50;
+  matTooltip = '50%';
+  value1 = 70;
+  matTooltip1 = '70%';
+  bufferValue = 100;
+
+  formatLabel(value: number) {
+    return value + '%';
+  }
   ngOnInit() {
   }
 

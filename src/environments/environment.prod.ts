317eebb00380b186c34cfe7371697c60fa5b296c
wilbrone <wilbroneokoth@gmail.com>
2020-10-28 18:17:34 +0300

    add environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBRYC8ILuoQV-23GwY2t4EB863-URMoK_w',
+    authDomain: 'myportfolio-70aaf.firebaseapp.com',
+    databaseURL: 'https://myportfolio-70aaf.firebaseio.com',
+    projectId: 'myportfolio-70aaf',
+    storageBucket: 'myportfolio-70aaf.appspot.com',
+    messagingSenderId: '281594915039',
+    appId: '1:281594915039:web:7738f37b190022c00ae16a',
+    measurementId: 'G-XYLBS9ETJ4'
+  },
 };
